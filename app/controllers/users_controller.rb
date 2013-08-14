d40c7e0c58bccf9703eff852d59aea4ce1f0e7f0
Graeme <graeme.barnes@cloudbi.co.za>
2013-08-14 12:29:02 +0200

    updating-users-broken

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -7,6 +9,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 
   def create
     @user = User.new(user_params)
@@ -25,4 +45,19 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    #before filters
+
+    def signed_in_user
+      unless  signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+  end
+
+     def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
