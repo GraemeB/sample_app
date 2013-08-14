e4ca4c7a09cf35449ad82749a646bbff61ef5e9c
Angus Miller <all4miller@gmail.com>
2013-08-14 13:10:49 +0200

    fix name in user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:user) { |n| "Person #{n}" }
+    sequence(:name) { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
